b479875fc632235c3fc855cfb8dd826a6befcadb
Lucifer516-sudo <harishvengat02092005@gmail.com>
2021-09-22 23:48:58 +0530

    Added two more files

--- September-21-2021/chapter-10-branched-statements/using_if_else_print_greatest_number.cpp
+++ September-21-2021/chapter-10-branched-statements/using_if_else_print_greatest_number.cpp
@@ -3,3 +3,15 @@
 #include <iostream>
 using namespace std;
 
+int main(){
+:	int n,r;
+	cout << "Enter number : ";
+	cin >> n;
+	r = n % 2;
+	if ( r == 0)
+		cout << n <<" is an Even Number";
+	else
+		cout << n << " is an Odd Number ";
+
+	return 0;
+}
